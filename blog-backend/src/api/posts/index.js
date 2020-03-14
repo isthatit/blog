5631d697628e01cbc51e700f77eaa2fbbaffe83b
isthatit <isthatitdev@gmail.com>
2020-03-14 22:23:05 +0900

    .gitignore add

---
+++ blog-backend/src/api/posts/index.js
@@ -0,0 +1,19 @@
+import Router from 'koa-router';
+
+import * as postsCtrl from './posts.ctrl';
+
+const posts = new Router();
+
+posts.get('/', postsCtrl.list);
+posts.post('/', postsCtrl.write);
+/* refactoring
+posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.read);
+posts.delete('/:id', postsCtrl.checkObjectId, postsCtrl.remove);
+posts.patch('/:id', postsCtrl.checkObjectId, postsCtrl.update);
+*/
+const post = new Router();
+post.get('/', postsCtrl.read);
+post.delete('', postsCtrl.remove);
+post.patch('/', postsCtrl.update);
+posts.use('/:id', postsCtrl.checkObjectId, post.routes());
+export default posts;
